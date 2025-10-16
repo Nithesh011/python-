234c7fe1ab588f486d153b5de00ac9a8040608e6
Nithesh Kumar S <97720688+Nithesh011@users.noreply.github.com>
2025-10-16 10:32:27 +0530

    Implement prime number checker function

---
+++ functions/prime_number_checker.py
@@ -0,0 +1,11 @@
+def find_prime(n:int):
+    if n < 1:
+        return f"Please enter the natural number to check prime"
+    i=2
+    while i*i <= n:
+        if n % i == 0:
+            return f"False :The given number {n} is not a prime number"
+        i += 1
+    return f"True:The given number {n} is a prime number"
+n=5
+print(find_prime(n))
